7cd4af8639fb75eeb05ba82d1b42016a7ee0072d
Nam Se Hyun <skatpgusskat@naver.com>
2016-04-15 06:05:49 +0000

    handle ie11 too.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
 
 function pleaseDoNotBotherUs(req, res, next) {
-  if(req.headers['user-agent'].indexOf("MSIE") >= 0) {
+  const userAgent = req.headers['user-agent'];
+  if(userAgent && (
+    userAgent.indexOf('MSIE') >= 0
+    || userAgent.indexOf('Trident') >= 0)) {
     return res.redirect("https://www.google.com/chrome/");
   }
   return next();
-};
+}
 
 module.exports = pleaseDoNotBotherUs.pleaseDoNotBotherUs = pleaseDoNotBotherUs.pleaseDoNotBotherUs = pleaseDoNotBotherUs;
